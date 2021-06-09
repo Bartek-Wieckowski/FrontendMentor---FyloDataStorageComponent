092edc929ad5fae8c310d9c8ec4088f5fb9fc8f4
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-09 18:52:55 +0200

    add function which calc number used and left

---
+++ main.js
@@ -0,0 +1,20 @@
+const numberLeft = document.querySelector(".number");
+// const numberUsed = document.querySelector(".numberUsed");
+
+const range = document.querySelector(".range");
+const numberUsed = document.querySelector(".numberUsed");
+
+range.addEventListener("input", () => {
+  setBubble(range, numberUsed);
+});
+setBubble(range, numberUsed);
+function setBubble(range, numberUsed) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  numberUsed.innerHTML = val;
+
+  const calcLeft = Number(range.max - range.value);
+  numberLeft.innerHTML = calcLeft;
+}
